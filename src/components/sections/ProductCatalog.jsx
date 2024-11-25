c6b095f5775f61b12fc6ec08216328421b4c2d0f
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2024-11-25 20:57:51 +0200

    second commit

--- src/components/sections/ProductCatalog.jsx
+++ src/components/sections/ProductCatalog.jsx
@@ -14,7 +14,6 @@ const ProductCatalog = () => {
           <ProductCard />
           <ProductCard />
           <ProductCard />
-          <ProductCard />
         </ul>
         <MainButton text="Смотреть всё" addStyles="mx-auto" />
       </Container>
